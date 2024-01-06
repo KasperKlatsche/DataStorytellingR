bc442c34fb4b5c819bd84dafa1264a9c6f364708
Julius <julius.heitmann@yahoo.de>
2024-01-06 10:44:30 +0100

    Bürokratiegraph fertiggestellt

--- code/bürokratieaufwandÜberZeitUndAmt.R
+++ code/bürokratieaufwandÜberZeitUndAmt.R
@@ -7,26 +7,47 @@ library(viridis)
 #Differenzen über die Jahre
 data <- read.table("./data/20231231_diff-jaehrl-erfuellungsaufw-wirt_StatBundAmt.csv", sep=";", header=T)
 
+#erst die summen ueber Zeit bilden
+data[1,2:ncol(data)] <- as.numeric(gsub(",",".",gsub("\\.","",data[1,2:ncol(data)])))
+for(i in 2:nrow(data)) {
+  data[i,2:ncol(data)] <- as.numeric(gsub(",",".",gsub("\\.","",data[i,2:ncol(data)]))) + as.numeric(data[i-1,2:ncol(data)])
+}
+
+#jetzt langweilige Ämter entfernen
+data <- data[,c(1,4,7:13,15)]
 aemter <- names(data)[2:ncol(data)]
 jahre <- data[,1]
+
 #daten vorbereiten
 auskombiniert <- cbind(unlist(data[,2:ncol(data)]), rep(jahre, length(aemter)), rep(aemter, each=length(jahre)))
 auskombiniert <- data.frame(auskombiniert)
 names(auskombiniert) <- c("aufwandDiff", "jahr", "Amt")
 #jetzt numerische werte erstellen
-auskombiniert$aufwandDiff <- as.numeric(gsub(",",".",auskombiniert$aufwandDiff))
+#auskombiniert$aufwandDiff <- as.numeric(gsub(",",".",auskombiniert$aufwandDiff))
+auskombiniert$aufwandDiff <- as.numeric(auskombiniert$aufwandDiff)
 auskombiniert$jahr <- as.numeric(auskombiniert$jahr)
-auskombiniert$Amt <- as.factor(auskombiniert$Amt)
-#alle Ämter fliegen raus, die leere Werte haben
-hatLeereWerte <- unique(auskombiniert$Amt[which(is.na(auskombiniert[,1]))])
-auskombiniert <- auskombiniert[-which(auskombiniert$Amt==hatLeereWerte),]
 
+#jetzt die Bundesämter bennenen
+auskombiniert$Amt[which(auskombiniert$Amt == "BMAS")] <- "... für Arbeit und Soziales"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMEL")] <- "... für Ernährung und Landwirtschaft"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMF")] <- "... der Finanzen"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMFSFJ")] <- "... für Familie, Senioren, Frauen und Jugend"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMG")] <- "... für Gesundheit"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMI")] <- "... des Inneren und für Heimat"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMJ")] <- "... der Justiz"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMUV")] <- "... für Umwelt, Naturschutz, nukleare Sicherheit und Verbraucherschutz"
+auskombiniert$Amt[which(auskombiniert$Amt == "BMWK")] <- "... für Wirtschaft und Klimaschutz "
+names(auskombiniert)[3] <- "Bundesministerium..."
 
-g <- ggplot(data = auskombiniert, aes(x=Amt, y=aufwandDiff, fill=Amt)) +
+g <- ggplot(data = auskombiniert, aes(x=Bundesministerium..., y=aufwandDiff, fill=Bundesministerium...)) +
   geom_bar(stat="identity") +
   scale_fill_viridis(option = "A", discrete = TRUE) +
   scale_color_viridis(option = "A", discrete = TRUE) + 
-  transition_time(jahr) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  ylab("Aggregated difference since 2012 [mio €]") +
+  transition_time(as.integer(jahr)) +
   labs(title = "Year: {frame_time}")
 print(g)
 
@@ -35,10 +56,18 @@ data <- read.table("./data/20240103_Absoluter Erfüllungsaufwand_StatBundAmt.csv
 #der header hat einen character, der nicht header sein darf, deshalb so
 names(data) <- data[1,]
 data <- data[-c(1),-c(34)]
+data[,18] <- as.numeric(gsub(",",".",gsub("\\.","",data[,18])))/1000
+data[,23] <- as.numeric(gsub(",",".",gsub("\\.","",data[,23])))/1000
+data[,4] <- as.factor(data[,4])
+data <- data[,c(4,18,23)]
+names(data) <- c("Ressort", "absErfAuf", "diffErfAuf")
+print(aggregate(diffErfAuf ~ Ressort, data, sum))
 
+cbind(1:ncol(data),names(data))
+summary(as.factor(substr(data[,7],7,11)))
 #aggregieren über Ressorts
 d
-print(tapply(data$))
+print(tapply(data[,23], data[,4], FUN=sum(na.rm=T)))
 
 #Es ist nur die Diff, nicht der totale Wert
 #Es fehlen Farben
