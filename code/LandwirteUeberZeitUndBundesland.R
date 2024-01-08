11bbd1599e97f0482c2f65557507cea4084516b3
Dr. Reinhold-Julius Heitmann | CORE <julius.heitmann@core.se>
2024-01-08 23:35:01 +0100

    Landwirtschaft nach Bundesland Graph

--- code/LandwirteUeberZeitUndBundesland.R
+++ code/LandwirteUeberZeitUndBundesland.R
@@ -4,8 +4,35 @@
 
 library(ggplot2)
 library(gganimate)
+library(stringr)
 
 data <- read.table("./data/20240108_Landwirtschaftlich Bundeslaender_statBundAmt.csv", sep=";", header=T, fileEncoding="latin1")
+#erst die Zahlen in Zahlen wandeln
 for(i in 2:ncol(data)){
-  data[,i] <- 
+  data[,i] <- as.numeric(gsub(",",".",data[,i]))
 }
+
+data10 <- data.frame(data$Bundesland, rep(2010, nrow(data)), data$X2010.Betriebe, data$X2010.Flaeche)
+data20 <- data.frame(data$Bundesland, rep(2020, nrow(data)), data$X2020.Betriebe, data$X2020.Flaeche)
+data21 <- data.frame(data$Bundesland, rep(2021, nrow(data)), data$X2021.Betriebe, data$X2021.Flaeche)
+data22 <- data.frame(data$Bundesland, rep(2022, nrow(data)), data$X2022.Betriebe, data$X2022.Flaeche)
+
+names(data10) <- c("Bundesland", "Jahr", "Betriebe", "Flaeche")
+names(data20) <- c("Bundesland", "Jahr", "Betriebe", "Flaeche")
+names(data21) <- c("Bundesland", "Jahr", "Betriebe", "Flaeche")
+names(data22) <- c("Bundesland", "Jahr", "Betriebe", "Flaeche")
+
+gesamt <- rbind(data10,data20,data21,data22)
+
+p <- ggplot(
+  gesamt, 
+  aes(x = Flaeche, y=Betriebe, colour = Bundesland)
+) +
+  geom_point(show.legend = T, alpha = 0.7) +
+  scale_fill_viridis(option = "rainbow", discrete = TRUE) +
+  scale_color_viridis(option = "rainbow", discrete = TRUE) + 
+  labs(x = "Flaeche", y = "Anz. Betriebe") +
+  scale_x_log10(labels = label_comma()) +
+  transition_time(as.Integer(Jahr)) +
+  labs(title = "Year: {frame_time}")
+print(p)
