73cbbb6f2143ad70ddc16a3a320accbe020d2c3f
Julius <julius.heitmann@yahoo.de>
2024-01-14 10:25:46 +0100

    Bahn Graph und Datensaetze

--- code/VerspaetungenZugtypUeberTag.R
+++ code/VerspaetungenZugtypUeberTag.R
@@ -8,6 +8,11 @@ library(ggpubr)
 library(gganimate)
 library(magick)
 
+#--------------- parameter -----------------------------------------------------
+von <- "07:00" #zeitfenster beginn
+bis <- "18:00" #zeitfenster ende
+skalaMax <- 150 #skala ende x achse
+
 #--------------- zuerst funktionen erstellen -----------------------------------
 
 aggregationNachZugTyp <- function(data) {
@@ -37,10 +42,10 @@ klassifikationVerkehr <- function(zugTyp) {
   indexDrittanbieter <- (1:length(zugTyp))[-c(indexNahverkehr, indexRegio, indexFernverkehr)]
   
   output <- character(length(zugTyp))
-  output[indexNahverkehr] <- "Nahverkehr"
-  output[indexRegio] <- "Regionalverkehr"
-  output[indexFernverkehr] <- "Fernverkehr"
-  output[indexDrittanbieter] <- "Drittanbieter"
+  output[indexNahverkehr] <- "Local transport"
+  output[indexRegio] <- "Regional transport"
+  output[indexFernverkehr] <- "Long distance tran."
+  output[indexDrittanbieter] <- "Non-DB providers"
   
   return(output)
 }
@@ -58,68 +63,98 @@ datenVorverarbeiten <- function(data) {
   data$geplAnkunft <- as.POSIXlt(paste(data$geplAnkunftTag, data$geplAnkunftZeit, sep=" "),format="%Y-%m-%d %H:%M")
   data$verzAnkunft <- as.POSIXlt(paste(data$verzAnkunftTag, data$verzAnkunftZeit, sep=" "),format="%Y-%m-%d %H:%M")
   
-  data <- data[,c(1:8,18:21,17)]
+  data$grund <- as.factor(data$grund)
+  
+  data <- data[,c(1:8,17:ncol(data))]
   
   #jetzt noch neue Spalten hinzufügen
   data$verspaetung <- as.numeric(data$verzAnkunft - data$geplAnkunft)/60
   return(data)
 }
 
-setwd("./data")
-unnormal <- read.csv("20231118_normalerSamstag.csv")
+unnormal <- read.csv("./data/20240112_DeutscheBahn_StreikFreitag_gekuerzt.csv")
 unnormal <- datenVorverarbeiten(unnormal)
+#noch kurz die ausfälle als numerisch modellieren
+unnormal$ausfNum <- as.numeric(rep(0,nrow(unnormal)))
+unnormal$ausfNum[unnormal$ausfall == "ausfall"] <- 1
 
-referenz <- read.csv("20231113_normalerMontag.csv")
+referenz <- read.csv("./data/20240109_DeutscheBahn_normalerDienstag_gekuerzt.csv")
 referenz <- datenVorverarbeiten(referenz)
 
 
-#--------------- jetzt gehts ans zeichnen --------------------------------------
+#--------------- zuerst die Ausfälle berechnen ----------------
+
+ausfaelleZugtyp <- data.frame(unnormal$ausfNum, klassifikationVerkehr(unnormal$zugTyp), unnormal$geplAnkunft)
+names(ausfaelleZugtyp) <- c("ausfall","zugTyp", "Plan")
+#jetzt noch das Zeitfenster von bis rausfiltern
+start <- as.POSIXct(paste(substr(ausfaelleZugtyp$Plan[1],1,10), von))
+ende <- as.POSIXct(paste(substr(ausfaelleZugtyp$Plan[1],1,10), bis))
+ausfaelleZugtyp <- ausfaelleZugtyp[which(ausfaelleZugtyp$Plan >= start & ausfaelleZugtyp$Plan <= ende),]
+print(mean(ausfaelleZugtyp$ausfall))
+print(aggregate(ausfall ~ zugTyp, ausfaelleZugtyp, mean))
+
+#--------------- verspätungstabelle berechnen ----------------------------------
 
 verspaetungen <- function(data) {
   verspVerkehr <- data.frame(klassifikationVerkehr(data$zugTyp),data$verspaetung, data$geplAnkunft)
   #alle Halte rauswerfen, die Startbahnhöfe sind und damit keine Verspätungen
   verspVerkehr <- verspVerkehr[-which(is.na(verspVerkehr[,2])),]
   names(verspVerkehr) <- c("Verkehrstyp","Verspaetung","Plan")
-  #jetzt noch das Zeitfenster von 0700 bis 1900 rausfiltern
-  start <- as.POSIXct(paste(substr(verspVerkehr$Plan[1],1,10), "07:00"))
-  ende <- as.POSIXct(paste(substr(verspVerkehr$Plan[1],1,10), "19:00"))
+  #jetzt noch das Zeitfenster von bis rausfiltern
+  start <- as.POSIXct(paste(substr(verspVerkehr$Plan[1],1,10), von))
+  ende <- as.POSIXct(paste(substr(verspVerkehr$Plan[1],1,10), bis))
   verspVerkehr <- verspVerkehr[which(verspVerkehr$Plan >= start & verspVerkehr$Plan <= ende),]
   return(verspVerkehr)
 }
 
-#parameter
-skalaMax <- 150
-
 unnormalVersp <- verspaetungen(unnormal)
 referenzVersp <- verspaetungen(referenz)
 
+#-------------- kurz die verspätungen pro Zugtyp berechnen ---------------------
+aUnn <- aggregate(Verspaetung ~ Verkehrstyp, unnormalVersp, mean)
+aRef <- aggregate(Verspaetung ~ Verkehrstyp, referenzVersp, mean)
+versp <- merge(aUnn,aRef,by="Verkehrstyp")
+names(versp) <- c("Verkehrstyp","unnormal","referenz")
+versp$weniger <- (versp$referenz-versp$unnormal)/versp$referenz
+print(versp)
+
+#-------------- jetzt Graphen zeichnen -----------------------------------------
+
+textSize <- 18
+farbwahl <- "turbo"
+
 g1 <- ggplot(data = unnormalVersp, aes(x = Verspaetung, y = Verkehrstyp, color = Verkehrstyp, fill = Verkehrstyp)) +
   geom_density_ridges(alpha = 0.8, scale = 5) +
-  scale_fill_viridis(option = "A", discrete = TRUE) +
-  scale_color_viridis(option = "A", discrete = TRUE) + 
+  scale_fill_viridis(option = farbwahl, discrete = TRUE) +
+  scale_color_viridis(option = farbwahl, discrete = TRUE) + 
   theme_few() +
-  theme(legend.position = "none", axis.title.y=element_blank()) +
+  theme(legend.position = "none", 
+        axis.title.y=element_blank(),
+        text = element_text(size=textSize)) +
   xlim(0,skalaMax) + 
   xlab("Verspätung in min") +
-  labs(title = 'Samstag: {frame_time}', x = 'Verspätung in min', y = 'Density') +
+  labs(title = 'Train Strike: {frame_time}', x = 'Delay in min', y = 'Density') +
   transition_time(Plan) +
   ease_aes('linear')
 g2 <- ggplot(data = referenzVersp, aes(x = Verspaetung, y = Verkehrstyp, color = Verkehrstyp, fill = Verkehrstyp)) +
   geom_density_ridges(alpha = 0.8, scale = 5) +
-  scale_fill_viridis(option = "A", discrete = TRUE) +
-  scale_color_viridis(option = "A", discrete = TRUE) + 
+  scale_fill_viridis(option = farbwahl, discrete = TRUE) +
+  scale_color_viridis(option = farbwahl, discrete = TRUE) + 
   theme_few() +
-  theme(legend.position = "none", axis.text.y=element_blank(), axis.title.y=element_blank()) +
+  theme(legend.position = "none", 
+        axis.text.y=element_blank(), 
+        axis.title.y=element_blank(),
+        text = element_text(size=textSize)) +
   xlim(0,skalaMax) + 
   xlab("Verspätung in min") +
-  labs(title = 'Montag: {frame_time}', x = 'Verspätung in min', y = 'Density') +
+  labs(title = 'Regular Weekday: {frame_time}', x = 'Delay in min', y = 'Density') +
   transition_time(Plan) +
   ease_aes('linear')
 
 #Die Gifs rendern
 duration <- 50
-unnormalGif <- animate(g1, duration = duration, width = 390, height = 300)
-referenzGif <- animate(g2, duration = duration, width = 300, height = 300)
+unnormalGif <- animate(g1, duration = duration, width = 520, height = 600)
+referenzGif <- animate(g2, duration = duration, width = 400, height = 600)
 
 #jetzt die Gifs zusammenführen - scheinbar nicht mehr nötig, da animate jetzt ein magick-image auswirft
 #unnormalGif <- image_read(unnormalGif)
@@ -131,4 +166,4 @@ for(i in 2:100){
   outputGif <- image_join(outputGif, combined)
 }
 
-print(outputGif)
+anim_save("./graphs/20240113_deutscheBahn_StreikVerspaetungen.gif", outputGif)
---
+++ code/bahndatensaetzeKuerzen_inZeitfenster.R
@@ -0,0 +1,23 @@
+#das Script nimmt einen zu langen Bahndatensatz (>100MB) und verkürzt ihn auf das Zeitfenster 0700-1900 in der Hoffnung, dass er dann klein genug ist für GIT
+
+#parameter zeitfenster
+von <- "07:00"
+bis <- "19:00"
+
+#woher kommt die Datei und wohin soll die Antwort geschrieben werden?
+pfad <- file.choose()
+outputPfad <- paste(substr(pfad,0,nchar(pfad)-4), "_gekuerzt.csv",sep="")
+
+#jetzt daten lesen und verarbeiten
+data <- read.csv(pfad)
+#dazu die Zeit als Datum konvertieren
+data$geplAnkunft <- as.POSIXlt(paste(data$geplAnkunftTag, data$geplAnkunftZeit, sep=" "),format="%Y-%m-%d %H:%M")
+data$geplAbfahrt <- as.POSIXlt(paste(data$geplAbfahrtTag, data$geplAbfahrtZeit, sep=" "),format="%Y-%m-%d %H:%M")
+#und das Zeitfenster von bis rausfiltern
+start <- as.POSIXct(paste(substr(data$geplAnkunft[1],1,10), von))
+ende <- as.POSIXct(paste(substr(data$geplAnkunft[1],1,10), bis))
+output <- data[which((data$geplAnkunft >= start & data$geplAnkunft <= ende) | is.na(data$geplAnkunft)),] #Ankunft zwischen 0700 und 1900
+output <- output[-which((output$geplAbfahrt < start | output$geplAbfahrt > ende) & is.na(output$geplAnkunft)),] #für alle Startpunkt Abfahrt zwischen 0700 un 1900
+
+#jetzt die datei wieder wegschreiben
+write.csv(output[,1:(ncol(data)-2)], file=outputPfad)
