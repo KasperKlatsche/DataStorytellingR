7560d8355be077aeb7c05979c93bd8f1ed720dfd
Julius <julius.heitmann@yahoo.de>
2024-01-18 21:10:10 +0100

    Schnee Datensätze

--- code/bahndatensaetzeKuerzen_inZeitfenster.R
+++ code/bahndatensaetzeKuerzen_inZeitfenster.R
@@ -14,8 +14,8 @@ data <- read.csv(pfad)
 data$geplAnkunft <- as.POSIXlt(paste(data$geplAnkunftTag, data$geplAnkunftZeit, sep=" "),format="%Y-%m-%d %H:%M")
 data$geplAbfahrt <- as.POSIXlt(paste(data$geplAbfahrtTag, data$geplAbfahrtZeit, sep=" "),format="%Y-%m-%d %H:%M")
 #und das Zeitfenster von bis rausfiltern
-start <- as.POSIXct(paste(substr(data$geplAnkunft[1],1,10), von))
-ende <- as.POSIXct(paste(substr(data$geplAnkunft[1],1,10), bis))
+start <- as.POSIXct(paste(substr(data$geplAnkunft[2],1,10), von)) #achtung es kann schon mal sein, dass das datum NA ist - dann einfach eine andere Zeile wählen
+ende <- as.POSIXct(paste(substr(data$geplAnkunft[2],1,10), bis)) #achtung es kann schon mal sein, dass das datum NA ist - dann einfach eine andere Zeile wählen
 output <- data[which((data$geplAnkunft >= start & data$geplAnkunft <= ende) | is.na(data$geplAnkunft)),] #Ankunft zwischen 0700 und 1900
 output <- output[-which((output$geplAbfahrt < start | output$geplAbfahrt > ende) & is.na(output$geplAnkunft)),] #für alle Startpunkt Abfahrt zwischen 0700 un 1900
 
