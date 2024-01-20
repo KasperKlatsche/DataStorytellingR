f5993ba6fcb609827fc1ad89b9c0dd4227288d8a
Dr. Reinhold-Julius Heitmann | CORE <julius.heitmann@core.se>
2024-01-20 18:37:29 +0100

    Debugging des Graphen für örtliche Verteilung der Bahnverspätungen

--- code/VerspaetungenOertlichUeberTag.R
+++ code/VerspaetungenOertlichUeberTag.R
@@ -9,6 +9,7 @@ datenVorverarbeiten <- function(data) {
   data$zugTyp <- as.factor(data$zugTyp)
   data$von <- as.factor(data$von)
   data$nach <- as.factor(data$nach)
+  data$ausfall <- as.factor(data$ausfall)
   
   #zeiten wieder zusammensetzen aus den daten und zeiten
   data$geplAbfahrt <- as.POSIXlt(paste(data$geplAbfahrtTag, data$geplAbfahrtZeit, sep=" "),format="%Y-%m-%d %H:%M")
@@ -16,12 +17,18 @@ datenVorverarbeiten <- function(data) {
   data$geplAnkunft <- as.POSIXlt(paste(data$geplAnkunftTag, data$geplAnkunftZeit, sep=" "),format="%Y-%m-%d %H:%M")
   data$verzAnkunft <- as.POSIXlt(paste(data$verzAnkunftTag, data$verzAnkunftZeit, sep=" "),format="%Y-%m-%d %H:%M")
   
-  data <- data[,c(1:8,18:21,17)]
+  data <- data[,c(2,4:5,19:23)]
   
   #jetzt noch neue verspaetung hinzufügen
   data$verspaetung <- as.numeric(data$verzAnkunft - data$geplAnkunft)/60
   data$verspaetung[data$verspaetung<0] <- 0
   data$verspaetung[is.na(data$verspaetung)] <- 0
+  
+  #die Zielvariable erstellen
+  data$nachPlan <- rep("on time", nrow(data))
+  data$nachPlan[data$verspaetung > 5] <- "delayed"
+  data$nachPlan[data$ausfall == "ausfall"] <- "cancelled"
+  data$nachPlan <- as.factor(data$nachPlan)
   
   #alle Zuege, die im Zielbahnhof angekommen sind und keine Abfahrt haben - raus
   data <- data[-which(is.na(data$geplAbfahrt)),]
@@ -39,8 +46,8 @@ data <- datenVorverarbeiten(data)
 #---------------- Graphen zeichnen ---------------------------------------------
 
 data$geplAbfahrt <- as.POSIXct(data$geplAbfahrt)
-g <- ggplot(data, aes(y=breite,x=laenge,color=verspaetung)) +
-  geom_jitter() +
+g <- ggplot(data, aes(y=breite,x=laenge)) +
+  geom_jitter(aes(colour=nachPlan, shape=nachPlan)) +
   borders(database = "world", regions = c("Germany"), xlim = c(6.071, 14.979), ylim = c(47.41, 54.91)) +
   theme_light() +
   transition_time(geplAbfahrt) +
