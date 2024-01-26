02c740b5f23f3beb395b7105a79a354f4254ef10
Dr. Reinhold-Julius Heitmann | CORE <julius.heitmann@core.se>
2024-01-26 12:28:59 +0100

    Ausbildungsgraph

--- code/AusbildungUeberBundeslaenderUndZeit.R
+++ code/AusbildungUeberBundeslaenderUndZeit.R
@@ -16,7 +16,9 @@ for(i in 2:4) {
 }
 for(i in 5:ncol(data)) {
   data[,i] <- as.numeric(data[,i])
+  data[which(is.na(data[,i])),i] <- 0
 }
+
 
 #jetzt die Tabelle umformen nach Bundesländer
 bundland <- data.frame(Bundesland=character(), Jahr=numeric(), Branche=factor(), Herkunft=factor(), Geschlecht=factor(), Anzahl=numeric())
@@ -26,9 +28,11 @@ for(i in 5:ncol(data)) {
   names(batch) <- c("Bundesland", "Jahr", "Branche", "Herkunft", "Geschlecht", "Anzahl")
   bundland <- rbind(bundland, batch)
 }
-bundland$Anzahl[which(is.na(bundland$Anzahl))] <- 0
 
-#umformen nach Branche
+#Aggregieren ueber Bundesländer
+summe <- apply(data[,5:ncol(data)], 1, sum)
+aggreg <- data.frame(data$Jahr, data$Branche, data$Herkunft, data$Geschlecht, summe)
+names(aggreg) <- c("Jahr", "Branche", "Herkunft", "Geschlecht", "Anzahl")
 
 
 #---------------- verschiedene Graphen zeichnen ---------------------------------------------------
@@ -39,9 +43,9 @@ g1 <- ggplot(data[which(data$Branche=="Insgesamt" & data$Herkunft!="Insgesamt" &
   geom_bar(stat="identity", position=position_dodge()) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   transition_time(as.integer(Jahr)) +
-  labs(title = "Ausbildungsverträge nach Herkunft in {frame_time}")
+  labs(title = "Ausbildungsverträge nach Bundesland und Herkunft in {frame_time}")
 print(g1)
-#nix spanendendes
+#nix spannendendes
 
 data <- bundland
 g2 <- ggplot(data[which(data$Branche=="Insgesamt" & data$Herkunft=="Insgesamt" & data$Geschlecht!="Insgesamt"),], 
@@ -49,7 +53,7 @@ g2 <- ggplot(data[which(data$Branche=="Insgesamt" & data$Herkunft=="Insgesamt" &
   geom_bar(stat="identity", position=position_dodge()) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   transition_time(as.integer(Jahr)) +
-  labs(title = "Ausbildungsverträge nach Geschlecht in {frame_time}")
+  labs(title = "Ausbildungsverträge nach Bundesland und Geschlecht in {frame_time}")
 print(g2)
 #möglicherweise die Erkenntnis, dass männlich rauf und runter geht, aber weiblich kontinuierlich runter
 
@@ -59,5 +63,22 @@ g3 <- ggplot(data[which(data$Branche!="Insgesamt" & data$Herkunft=="Insgesamt" &
   geom_bar(stat="identity") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   transition_time(as.integer(Jahr)) +
-  labs(title = "Ausbildungsverträge nach Branche in {frame_time}")
+  labs(title = "Ausbildungsverträge nach Bundesland und Branche in {frame_time}")
 print(g3)
+#nix spannendes
+
+data <- aggreg
+g4 <- ggplot(data[which(data$Branche!="Insgesamt" & data$Herkunft=="Insgesamt" & data$Geschlecht!="Insgesamt"),], 
+             aes(x=Branche, y=Anzahl, fill=Geschlecht)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        text = element_text(size=30),
+        legend.title = element_text(colour="black", size = 30, face = "bold"),
+        legend.text  = element_text(colour="black", size = 30)) +
+  transition_time(as.integer(Jahr)) +
+  labs(title = "Ausbildungsverträge nach Branche und Geschlecht in {frame_time}")
+#dieser Graph scheint interessante Aussagen zu beinhalten - nehmen wir
+animate(g4, width = 1100, height = 1200)
+anim_save("./graphs/20240126_AusbildungsvertraegeBrancheGeschlecht.gif")
+
