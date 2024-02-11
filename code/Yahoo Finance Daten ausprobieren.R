44083d69c6e639793ec8753189f774c6a8106288
Dr. Reinhold-Julius Heitmann | CORE <julius.heitmann@core.se>
2024-02-11 09:30:02 +0100

    netflix graph - los gehts

--- code/Yahoo Finance Daten ausprobieren.R
+++ code/Yahoo Finance Daten ausprobieren.R
@@ -2,5 +2,7 @@
 
 library(yahoofinancer)
 
-data <- Ticker$new('nflx') #Netflix
+data <- Ticker$new('NVDA') #NVIDIA
 data$get_history(start = '2018-01-01', interval = '1d')
+
+print(data[((nrow(data)-100):nrow(data)),])
